83f48236846496d54a170e5b738d560803010e6e
Kaiqgs <kags_@live.com>
2019-05-03 14:29:09 -0300

    Gunicorn Fix

--- run.py
+++ run.py
@@ -1,56 +1,60 @@
 from app import manager, app
+from flask_script import Command
 
 class GunicornServer(Command):
-description = 'to run the app within Gunicorn'
+	description = 'to run the app within Gunicorn'
 
-def __init__(self, host='0.0.0.0', port=5000, workers=2):
-    self.port = port
-    self.host = host
-    self.workers = workers
+	def __init__(self, host='0.0.0.0', port=5000, workers=2):
+	    self.port = port
+	    self.host = host
+	    self.workers = workers
 
-def get_options(self):
-    return (
-        Option('-H', '--host',
-               dest='host',
-               default=self.host),
+	def get_options(self):
+	    return (
+	        Option('-H', '--host',
+	               dest='host',
+	               default=self.host),
 
-        Option('-p', '--port',
-               dest='port',
-               type=int,
-               default=self.port),
+	        Option('-p', '--port',
+	               dest='port',
+	               type=int,
+	               default=self.port),
 
-        Option('-w', '--workers',
-               dest='workers',
-               type=int,
-               default=self.workers),
-    )
+	        Option('-w', '--workers',
+	               dest='workers',
+	               type=int,
+	               default=self.workers),
+	    )
 
-def handle(self, app, host, port, workers):
+	def handle(self, app, host, port, workers):
 
-    from gunicorn import version_info
-    if version_info < (0, 9, 0):
-        from gunicorn.arbiter import Arbiter
-        from gunicorn.config import Config
-        arbiter = Arbiter(
-            Config(
-                {'bind': "%s:%d" % (host, int(port)), 'workers': workers}
-            ),
-            app
-        )
-        arbiter.run()
-    else:
-        from gunicorn.app.base import Application
+	    from gunicorn import version_info
+	    if version_info < (0, 9, 0):
+	        from gunicorn.arbiter import Arbiter
+	        from gunicorn.config import Config
+	        arbiter = Arbiter(
+	            Config(
+	                {'bind': "%s:%d" % (host, int(port)), 'workers': workers}
+	            ),
+	            app
+	        )
+	        arbiter.run()
+	    else:
+	        from gunicorn.app.base import Application
 
-        class FlaskApplication(Application):
-            def init(self, parser, opts, args):
-                return {
-                    'bind': '{0}:{1}'.format(host, port),
-                    'workers': workers
-                }
+	        class FlaskApplication(Application):
+	            def init(self, parser, opts, args):
+	                return {
+	                    'bind': '{0}:{1}'.format(host, port),
+	                    'workers': workers
+	                }
 
-            def load(self):
-                return app
+	            def load(self):
+	                return app
 
-        FlaskApplication().run()
+	        FlaskApplication().run()
 
+
+
+#Adding gunicorn based runserver command
 manager.add_command("gunicorn", GunicornServer())
