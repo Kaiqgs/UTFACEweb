70d21e7462ac2785c0ff9c94bc5acfc54f7d4361
Kaique Santos <kags_@live.com>
2019-05-10 09:33:12 -0300

    Updaten fixes

--- run.py
+++ run.py
@@ -1,25 +1,4 @@
-from app import manager
-from flask_script import  Command, Option
+from app import manager, app
 
-class GunicornServer(Command):
-    """Run the app within Gunicorn"""
-
-    def get_options(self):
-        from gunicorn.config import make_settings
-
-        settings = make_settings()
-        options = (
-            Option(*klass.cli, action=klass.action)
-            for setting, klass in settings.iteritems() if klass.cli
-        )
-        return options
-
-    def run(self, *args, **kwargs):
-        from gunicorn.app.wsgiapp import WSGIApplication
-
-        app = WSGIApplication()
-        app.app_uri = 'manage:app'
-        return app.run()
-
-manager.add_command("gunicorn", GunicornServer())
-manager.run()
+if __name__ == '__main__':
+	manager.run()
