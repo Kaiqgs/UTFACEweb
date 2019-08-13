0da6c46a7070226f19d8d293f75d3160fe9fafa8
Kaique Santos <kags_@live.com>
2019-08-13 13:45:10 -0300

    temp

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,41 +1,51 @@
 import datetime
-from app import flask_app, resp,db
-from flask import render_template,flash,redirect,url_for
+from app import flask_app, resp, db
+from flask import render_template, flash, redirect, url_for
 from app.models.forms import Contact
 import app.models.tables as tbs
 
-gradeChoices = [        ('1º Ano','1º Ano'),
-                        ('2º Ano','2º Ano'),
-                        ('3º Ano','3º Ano'),
-                        ('4º Ano','4º Ano'),
-                        ('5º Ano','5º Ano'),
-                        ('6º Ano','6º Ano'),
-                        ('7º Ano','7º Ano'),
-                        ('8º Ano','8º Ano'),
-                        ('9º Ano','9º Ano'),
-                        ('1º Ensino Med.','1º Ensino Med.'),
-                        ('2º Ensino Med.','2º Ensino Med.'),
-                        ('3º Ensino Med.','3º Ensino Med.')][::-1]
+gradeChoices = [('1º Ano', '1º Ano'),
+                ('2º Ano', '2º Ano'),
+                ('3º Ano', '3º Ano'),
+                ('4º Ano', '4º Ano'),
+                ('5º Ano', '5º Ano'),
+                ('6º Ano', '6º Ano'),
+                ('7º Ano', '7º Ano'),
+                ('8º Ano', '8º Ano'),
+                ('9º Ano', '9º Ano'),
+                ('1º Ensino Med.', '1º Ensino Med.'),
+                ('2º Ensino Med.', '2º Ensino Med.'),
+                ('3º Ensino Med.', '3º Ensino Med.')][::-1]
 
 
-@flask_app.route("/", methods=["GET","POST"])
-@flask_app.route("/home", methods=["GET","POST"])
+@flask_app.route("/", methods=["GET", "POST"])
+@flask_app.route("/home", methods=["GET", "POST"])
 def home():
-    return contactValidateRender(template_name_or_list = "index.html", sideBarActive = False)
+    return contactValidateRender(template_name_or_list="index.html", sideBarActive=False)
 
 
-@flask_app.route("/boas-praticas", methods=["GET","POST"])
+@flask_app.route("/boas-praticas", methods=["GET", "POST"])
 def praticas():
-    return contactValidateRender(template_name_or_list = "praticas.html", sideBarActive = True)
-    
+    return contactValidateRender(template_name_or_list="praticas.html", sideBarActive=True)
 
-@flask_app.route("/ferramentas-consumo", methods=["GET","POST"])
+
+@flask_app.route("/test", methods=["GET"])
+def data():
+    s = "Starts here:<br>"
+    for row in tbs.Contact.query.all():
+        s += f"{row.source:^100} : {row.message:^50}<br>"
+    return s
+
+
+@flask_app.route("/ferramentas-consumo", methods=["GET", "POST"])
 def consumo():
-    return contactValidateRender(template_name_or_list = "consumo.html", sideBarActive = True)
+    return contactValidateRender(template_name_or_list="consumo.html", sideBarActive=True)
 
-@flask_app.route("/ferramentas-criacao", methods=["GET","POST"])
+
+@flask_app.route("/ferramentas-criacao", methods=["GET", "POST"])
 def criacao():
-    return contactValidateRender(template_name_or_list = "criacao.html", sideBarActive = True)
+    return contactValidateRender(template_name_or_list="criacao.html", sideBarActive=True)
+
 
 def contactValidateRender(**kwargs):
     cnt = Contact()
@@ -43,11 +53,11 @@ def contactValidateRender(**kwargs):
     kwargs["form"] = cnt
     if cnt.validate_on_submit():
         contact = tbs.Contact(email=cnt.email.data,
-                    name=cnt.name.data,
-                    age = cnt.age.data,
-                    grade = cnt.grade.data,
-                    message = cnt.message.data,
-                    source = cnt.source.data)
+                              name=cnt.name.data,
+                              age=cnt.age.data,
+                              grade=cnt.grade.data,
+                              message=cnt.message.data,
+                              source=cnt.source.data)
         db.session.add(contact)
         db.session.commit()
         print(f"New contact made: {cnt.name.data} was added.")
